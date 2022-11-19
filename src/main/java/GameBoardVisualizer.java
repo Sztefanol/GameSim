4cb457f1811d7ea5b2a3b0e4fad6d093b5d6c9f7
Sztefanol <luk266@gmail.com>
2022-11-19 09:52:34 +0100

    Implement better console GameBoard visualization.

--- src/main/java/GameBoardVisualizer.java
+++ src/main/java/GameBoardVisualizer.java
@@ -1,16 +1,32 @@
 public class GameBoardVisualizer {
   public static String toString(GameBoard gameBoard) {
 
+    String HORIZONTAL_LINE = "\n-------\n";
+
     GameBoardSymbols[][] board = gameBoard.getBoard();
     StringBuilder result = new StringBuilder();
+    String currentTextSymbol = "";
 
+    result.append(HORIZONTAL_LINE);
     for (int i = 0; i < board.length; i++) {
+      result.append("|");
       for (int j = 0; j < board.length; j++) {
-        result.append(board[i][j].toString());
-        result.append(" ");
+        switch (board[i][j]) {
+          case CIRCLE -> result.append("O|");
+          case CROSS -> result.append("X|");
+          case EMPTY -> result.append(" |");
+        }
       }
-      result.append("\n");
+      result.append(HORIZONTAL_LINE);
     }
+
+//    for (int i = 0; i < board.length; i++) {
+//      for (int j = 0; j < board.length; j++) {
+//        result.append(board[i][j].toString());
+//        result.append(" ");
+//      }
+//      result.append("\n");
+//   }
     return result.toString();
   }
 }
