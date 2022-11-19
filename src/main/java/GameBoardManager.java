879cfde9eae9b9e9b6a32ff07dd02034ab004064
Sztefanol <luk266@gmail.com>
2022-11-19 09:50:33 +0100

    Implement game logic.

--- src/main/java/GameBoardManager.java
+++ src/main/java/GameBoardManager.java
@@ -5,6 +5,7 @@ public class GameBoardManager {
   private final GameBoard gameBoard;
   private final Player[] players = new Player[2];
   private int currentTurnPlayerNumber = 1;
+  private GameState gameState = GameState.IN_GAME;
 
   public GameBoardManager(Player player1, Player player2, GameBoard gameBoard){
     this.gameBoard = gameBoard;
@@ -13,8 +14,47 @@ public class GameBoardManager {
   }
 
   public void gameStep() {
+    if(gameState != GameState.IN_GAME){
+      return;
+    }
 
+    int[] currentMove = players[currentTurnPlayerNumber - 1].makeMove(gameBoard.getBoard());
+    int x = currentMove[0];
+    int y = currentMove[1];
+    GameBoardSymbols currentPlayerSymbol = GameBoardSymbols.values()[currentTurnPlayerNumber];
+
+    if(!TicTacToeRuleChecker.isMoveValid(x, y, gameBoard)) {
+      return;
+    }
+
+    gameBoard.addMove(x, y, currentPlayerSymbol);
+
+    if(TicTacToeRuleChecker.isWinningMove(x, y, currentPlayerSymbol, gameBoard)) {
+      gameState = GameState.values()[currentTurnPlayerNumber];
+      return;
+    }
+
+    if(gameBoard.isBoardFull()) {
+      gameState = GameState.DRAW;
+      return;
+    }
+
+    switchPlayers();
   }
 
+  private void switchPlayers() {
+    if (currentTurnPlayerNumber == 1) {
+      currentTurnPlayerNumber = 2;
+    } else {
+      currentTurnPlayerNumber = 1;
+    }
+  }
 
+  public int getCurrentTurnPlayerNumber() {
+    return currentTurnPlayerNumber;
+  }
+
+  public GameState getGameState() {
+    return gameState;
+  }
 }
