cfb0b94d266855bf7c03d104e2897d608aac50aa
Sztefanol <luk266@gmail.com>
2022-11-20 08:47:29 +0100

    Proof of concept working gameloop.

--- src/main/java/GameRun.java
+++ src/main/java/GameRun.java
@@ -1,7 +1,31 @@
-public class GameRun {
-  public static void main(String[] args) {
-    GameBoard gameboard = new GameBoard(3);
-    System.out.println(GameBoardVisualizer.toString(gameboard));
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class GameRun {
+
+  public static void main(String[] args) throws IOException {
+    GameBoard gameboard = new GameBoard(3);
+    GameBoardManager gameBoardManager = new GameBoardManager(new RandomPlayer(), new RandomPlayer(),
+        gameboard);
+    InputStreamReader reader = new InputStreamReader(System.in);
+
+    while (gameBoardManager.getGameState() == GameState.IN_GAME) {
+      System.out.println("Ruch wykonuje gracz nr " + gameBoardManager.getCurrentTurnPlayerNumber());
+
+      switch (gameBoardManager.gameStep()) {
+        case MOVE_OK -> System.out.println("Gracz wykonał prawidłowy ruch");
+        case ILLEGAL_MOVE -> System.out.println("Gracz wykonał niedozwolony ruch");
+        case GAME_END -> System.out.println("Koniec gry");
+      }
+
+      System.out.println(GameBoardVisualizer.toString(gameboard));
+      reader.read();
     }
+
+    switch (gameBoardManager.getGameState()) {
+      case DRAW -> System.out.println("Remis");
+      case PLAYER_1_WON -> System.out.println("Gracz 1 wygrał");
+      case PLAYER_2_WON -> System.out.println("Gracz 2 wygrał");
+    }
+  }
 }
